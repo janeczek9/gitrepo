608212be295d295e52421bccfcf7af32adbb8bf4
janeczek9 <jan.ziolo@lo1.sandomierz.pl>
2019-09-16 10:35:12 +0200

    algorytmy w c++ cd.

---
+++ cpp/petla_while.cpp
@@ -0,0 +1,25 @@
+/*
+ * petla.cpp
+ * 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{   
+	int i = 0; // dEKLARACJA I INICJACJA
+    while(i <= 20 )  {
+        cout << i <<endl;
+        cout << "Hello!";
+        i += 2;
+    
+}
+
+    cout << "Wartość i: " << i << endl;
+    
+	return 0;
+}
+
