f21c3a2003e9a1c4eafd9f0e4e5575a51857b1a6
janeczek9 <jan.ziolo@lo1.sandomierz.pl>
2019-10-07 10:35:15 +0200

    funkcje

---
+++ cpp/cwiczenia_funkcje.cpp
@@ -0,0 +1,41 @@
+/*
+ * cwiczenia_funkcje.cpp
+ * 
+ * Copyright 2019  <>
+
+ */
+
+
+#include <iostream>
+using namespace std;
+
+
+
+int drukuj(a, b)   {
+    cout << "Pracujesz " << a << "lat i zarabiasz " << b << endl;
+    return 0
+    }
+int awans(a, b) {
+    
+    
+    }
+
+
+
+int cw1()   {
+    int staz = 1;
+    int zarobek = 1000;
+    int lata = 0
+    cout << "Jaki jest przewidywany okres zatrudnienia (w latach): " << endl
+    cin << lata;
+    drukuj(staz, zarobek)
+    return 0;
+    }
+
+
+int main(int argc, char **argv)
+{
+	cw1();
+	return 0;
+}
+
---
+++ cpp/funkcje2.cpp
@@ -0,0 +1,58 @@
+/*
+ * funkcje2.cpp
+ *  
+ * Copyright 2019  <>
+    
+ */ 
+    
+    
+#include <iostream>
+    
+using namespace std;
+
+int b = 10;
+
+int dodaj() {
+    int a = 0;
+    cout << a << endl;
+    cout << "b = " << b;
+    return 0;
+    }
+    
+int dodaj2(int a, int b) {
+    //a = 20;
+    //cout << "a = " << a << endl;
+    //b = 50;
+    //cout << "b = " << b << endl;
+    //cout << a + b << endl;
+    return a + b;
+    }
+    
+void modyfikuj(int &x, int &y, int &z)    {
+    cout << &z << endl;
+    x = 2000;
+    y = 2000;
+    z = x + y + 50;
+    
+    
+    }    
+
+    
+int main(int argc, char **argv)
+{
+    int a = 1;
+    int b = 200;
+    //cout << a << endl;
+    //cout << b << endl;
+    int suma = dodaj2(a, b);
+    //cout << "Suma = " << suma << endl;
+    cout << "Suma = " << suma << endl;
+    modyfikuj(a, b, suma);
+    cout << a << endl;
+    cout << b << endl;
+    cout << suma << endl;
+	return 0;
+    
+}   
+
+
