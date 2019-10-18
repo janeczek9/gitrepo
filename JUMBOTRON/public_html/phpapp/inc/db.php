70e86ae774405393c3d2fbb83c81523d5f9ba83c
janeczek9 <jan.ziolo@lo1.sandomierz.pl>
2019-10-18 09:41:47 +0200

    bazy

--- JUMBOTRON/public_html/phpapp/inc/db.php
+++ JUMBOTRON/public_html/phpapp/inc/db.php
@@ -1,3 +1,11 @@
 <?php
+function inti_baza(){
+	global $db, $dbfile;
+	if (!file_exists(DBASE.$dbfile))	{
+		$kom[] = 'Brak pliku bazy, Tworzę nowy.';
+		$db = new PDO("sqlite:DBASE.$dbfile"):
+	}
+	$db->setAttribute(PDO::ATTR_ERRMODE, POD::ERRMODE_EXCEPTION);
+}
 ;
 ?>
